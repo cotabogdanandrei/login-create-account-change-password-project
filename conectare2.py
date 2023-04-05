c8f7fde7074386f31e95b40c1f3e9351b9ceaf61
cotabogdanandrei <33202997+cotabogdanandrei@users.noreply.github.com>
2023-04-05 18:59:18 +0300

    Update conectare2.py

--- conectare2.py
+++ conectare2.py
@@ -3,146 +3,125 @@ from tkinter import messagebox
 from PIL import ImageTk
 import pymysql
 
-def conectare2_page():
-    login_window.destroy()
-    import conectare2
+def clear():
+    emailEntry.destroy()
+    usernameEntry.destroy()
+    parolaEntry.destroy()
+    confirmaparolaEntry.destroy()
+    check.set(0)
 
+def login_page():
+    singup_window.destroy()
+    import conectare
 
-def forget_pass():
-    def change_parola():
-        if user_entry.get()=='' or parolanew_entry.get()=='' or confirmaparola_entry.get()=='':
-            messagebox.showerror('Eroare','Toate spatiile sunt necesare',parent=window)
-        elif parolanew_entry.get() !=confirmaparola_entry.get():
-            messagebox.showerror('Eroare','Parola si confirma parola nu coincid',parent=window)
-        else:
-            conn = pymysql.connect(host='localhost', user='root', password='280802',database='login')
-            mycursor = conn.cursor()
-            query ='SELECT * FROM data where username=%s'
-            mycursor.execute(query,(user_entry.get()))
-            row=mycursor.fetchone()
-            if row==None:
-                messagebox.showerror('Eroare','Username incorect',parent=window)
-            else:
-                query='update data set parola=%s where username=%s'
-                mycursor.execute(query,(parolanew_entry.get(),user_entry.get()))
-                conn.commit()
-                conn.close()
-                messagebox.showinfo('Succes','Parola este schimbata, acum te poti loga',parent=window)
-                window.destroy()
-    window = Toplevel()
-    window.title('Schimba Parola')
-
-    bgPic = ImageTk.PhotoImage(file='parola.jpg')
-    bgLabel = Label(window, image=bgPic)
-    bgLabel.grid()
-
-    heading_label = Label(window, text='RESETEAZA PAROLA', font=('arial','19','bold',),bg='deep sky blue', fg='black')
-    heading_label.place(x=100, y=150)
-
-    userLabel = Label(window, text='Username', font=('arial',13, 'bold'),bg='deep sky blue',fg='black')
-    userLabel.place(x=100, y=210)
-
-    user_entry = Entry(window, width=26, fg='black', font=('arial',11,'bold'),bd=0)
-    user_entry.place(x=100, y=240)
-
-    Frame(window, width=210, height=2, bg='deep sky blue').place(x=100, y=260)
-
-    parolanewlabel = Label(window, text='Parola noua', font=('arial',13,'bold'),bg='deep sky blue',fg='black')
-    parolanewlabel.place(x=100, y=270)
-
-    parolanew_entry = Entry(window, width=26, fg='black', font=('arial',11,'bold'),bd=0)
-    parolanew_entry.place(x=100, y=295)
-
-    Frame(window, width=210, height=2, bg='deep sky blue').place(x=100, y=315)
-
-    confirmaparolalabel = Label(window, text='Confirma parola', font=('arial',13,'bold'),bg='deep sky blue',fg='black')
-    confirmaparolalabel.place(x=100, y=325)
-
-    confirmaparola_entry = Entry(window, width=26, fg='black', font=('arial',11,'bold'),bd=0)
-    confirmaparola_entry.place(x=100, y=346)
-
-    Frame(window, width=210, height=2, bg='deep sky blue').place(x=100, y=365)
-
-    confirmaButton = Button(window, text='Confirma',bd=0, bg='deep sky blue', fg='black', font=('Open Sans','16','bold'),width=25, cursor='hand2', activebackground='deep sky blue', activeforeground='white',command=change_parola)
-
-    confirmaButton.place(x=100, y=400)
-
-
-    window.mainloop()
-
-
-
-def login_user():
-    if usernameEntry.get()=='' or parolaEntry.get()=='':
-        messagebox.showerror('Eroare','Completati toate campurile')
+def connect_login():
+    if emailEntry.get()=='' or usernameEntry.get()=='' or parolaEntry.get()=='' or confirmaparolaEntry.get()=='':
+        messagebox.showerror('Eroare' , 'Toate cerintele nu sunt acceptate')
+    elif parolaEntry.get() != confirmaparolaEntry.get():
+        messagebox.showerror('Eroare', 'Parolele nu coincid')
+    elif check.get()==0:
+        messagebox.showerror('Eroare', 'Nu ai acceptat termeni si conditiile')
     else:
         try:
-            conn = pymysql.connect(host='localhost', user='root', password='280802',db='login')
+            conn = pymysql.connect(host='localhost' , user='root' , password='280802' , db='login')
             mycursor=conn.cursor()
         except:
-            messagebox.showerror('Eroare','Baza de date este offline')
+            messagebox.showerror('Eroare' , 'Problema cu baza de date,Incearca mai tarziu')
             return
-        query = 'use login'
-        mycursor.execute(query)
-        query='select * from data where username=%s and parola=%s'
-        mycursor.execute(query, (usernameEntry.get(), parolaEntry.get()))
+        try:
+            query='create login'
+            mycursor.execute(query)
+            query='use login'
+            mycursor.execute(query)
+            query='CREATE TABLE data(id int NOT NULL AUTO_INCREMENT,email varchar(50),username varchar(100),parola varchar(20))'
+            mycursor.execute(query)
+        except:
+            mycursor.execute('use login')
+        query=('SELECT * FROM data where username=%s')
+        mycursor.execute(query , (usernameEntry.get()))
+
         row=mycursor.fetchone()
-        if row==None:
-            messagebox.showerror('Eroare','Nume sau parola incorecta')
+        if row !=None:
+            messagebox.showerror('Eroare','Acest utilizator exista')
         else:
-            messagebox.showinfo('Succes','Te-ai conectat cu succes')
+            query=('INSERT INTO data(email,username,parola) VALUES (%s,%s,%s)')
+            mycursor.execute(query , (emailEntry.get() , usernameEntry.get() , parolaEntry.get()))
 
-def user_enter(event):
-    if usernameEntry.get()=='Username':
-        usernameEntry.delete(0,END)
-def parola_enter(event):
-    if parolaEntry.get()=='Password':
-        parolaEntry.delete(0,END)
-
-
-login_window=Tk()
-
-background=ImageTk.PhotoImage(file='poza6.png')
-
-bgLabel=Label(login_window,image=background)
-bgLabel.grid(row=100,column=100)
-
-login_window.resizable(10,10)
-login_window.title('Pagina de autentificare')
+        conn.commit()
+        conn.close()
+        messagebox.showinfo('Succes','Inregistrare cu succes')
+        clear()
+        singup_window.destroy()
+        import conectare
 
 
-heading=Label(login_window,text='Autentificare operator',font=('Microsoft Yahei UI Light',23,'bold'),fg='black',bg='purple2')
+singup_window=Tk()
 
-heading.place(x=200,y=120)
+background=ImageTk.PhotoImage(file='nori.jpg')
 
-usernameEntry=Entry(login_window,width=35,font=('Microsoft Yahei UI Light',11,'bold'),bd=0,fg="black",bg='purple2')
-usernameEntry.place(x=200,y=210)
-usernameEntry.insert(0,'Username')
+bgLabel = Label(singup_window,image=background)
+bgLabel.grid()
 
-usernameEntry.bind('<FocusIn>',user_enter)
+singup_window.title('Creare cont')
 
-frame1=Frame(login_window,width=320,height=2,bg='black').place(x=200,y=230)
+frame=Frame(singup_window)
+frame.place(x=80,y=50)
 
-parolaEntry=Entry(login_window,width=35,font=('Microsoft Yahei UI Light',11,'bold'),bd=0,fg="black",bg='purple2')
-parolaEntry.place(x=200,y=260)
-parolaEntry.insert(0,'Parola')
+heading=Label(singup_window,text='Creaza un cont' , font=('Microsoft Yahei UI Light' , 21 , 'bold') , fg='black' , bg='white')
+heading.place(x=80 , y=50)
 
-parolaEntry.bind('<FocusIn>',parola_enter)
+emailLabel=Label(frame , text='Email' , font=('Microsoft Yahei UI Light',10,'bold'),bg='white')
 
-frame2=Frame(login_window,width=320,height=2,bg='black').place(x=200,y=280)
+emailLabel.grid(row=1 , column=0 , sticky='w' , padx=25 , pady=(10 , 0))
 
+frame.place(x=80,y=100)
 
-forgetButton=Button(login_window,text='Ai uitat parola?',bd=0,bg='purple2',activebackground='white',cursor='hand2',font=('Microsoft Yahei UI Light',9,'bold'),fg="black",command=forget_pass)
-forgetButton.place(x=400,y=290)
+emailEntry=Entry(frame,width=25,font=('Microsoft Yahei UI Light',10,'bold'),bg='white')
 
-loginButton=Button(login_window,text='Login',font=('Open Sans',16,'bold'),fg='white',bg='purple2',activeforeground='white',cursor='hand2',bd=0,width=19,command=login_user)
-loginButton.place(x=200,y=330)
-
-singupLabel=Label(login_window,text='Nu ai cont?',font=('Open Sans',9,'bold'),fg='white',bg='purple2')
-singupLabel.place(x=200, y=290)
-
-newaccountButton=Button(login_window, text='Creaza un cont',font=('Open Sans',9,'bold underline'),fg='white',bg='purple2',activeforeground='white',activebackground='purple2',cursor='hand2',bd=0,command=conectare2_page)
-newaccountButton.place(x=269 , y=290)
+emailEntry.grid(row=2,column=0,sticky='w',padx=25)
 
 
-login_window.mainloop()
+usernameLabel=Label(frame,text='Utilizator',font=('Microsoft Yahei UI Light',10,'bold'),bg='white')
+
+usernameLabel.grid(row=3,column=0,sticky='w',padx=25,pady=(10,0))
+
+usernameEntry=Entry(frame,width=25,font=('Microsoft Yahei UI Light',10,'bold'),bg='white')
+
+usernameEntry.grid(row=4,column=0,sticky='w',padx=25)
+
+frame.place(x=80,y=100)
+
+parolaLabel=Label(frame,text='Parola',font=('Microsoft Yahei UI Light',10,'bold'),bg='white')
+
+parolaLabel.grid(row=5,column=0,sticky='w',padx=25,pady=(10,0))
+
+parolaEntry=Entry(frame,width=25,font=('Microsoft Yahei UI Light',10,'bold'),bg='white')
+
+parolaEntry.grid(row=6,column=0,sticky='w',padx=25)
+
+
+confirmaparolaLabel=Label(frame,text='Confirma Parola',font=('Microsoft Yahei UI Light',10,'bold'),bg='white')
+
+confirmaparolaLabel.grid(row=7,column=0,sticky='w',padx=25,pady=(10,0))
+
+confirmaparolaEntry=Entry(frame,width=25,font=('Microsoft Yahei UI Light',10,'bold'),bg='white')
+
+confirmaparolaEntry.grid(row=8,column=0,sticky='w',padx=25)
+
+check=IntVar()
+termeni=Checkbutton(frame,text='Accept termeni si conditiile',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',variable=check)
+termeni.grid(row=9,column=0,pady=10,padx=15)
+
+
+singupButton=Button(frame,text='Singup',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',bd=0,fg='black',width=20,command=connect_login)
+singupButton.grid(row=10,column=0,pady=10)
+
+aidejacont=Label(frame,text='Ai deja un cont?',font=('Open Sans',9,'bold'),bg='white')
+aidejacont.grid(row=11,column=0,sticky='w,',padx=25,pady=10)
+
+loginButton=Button(frame,text='Conecteaza-te',font=('Open Sans',9,'bold underline'),bg='white',bd=0,cursor='hand2',activebackground='red',command=login_page)
+loginButton.place(x=120,y=340)
+
+
+
+singup_window.mainloop()
